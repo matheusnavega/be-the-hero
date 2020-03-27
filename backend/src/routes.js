a456db72bcd373410fe3bec732bb4239e44e823f
Matheus Brito <matheus.brito@radixeng.com.br>
2020-03-27 00:19:33 -0300

    Back-end, front-end

---
+++ backend/src/routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query params: filtros, paginacao)
+ * Route params: identificar recursos
+ * Request body: criar recursos
+ * 
+ * */
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);  
+
+routes.get('/profile', ProfileController.index);
+    
+module.exports = routes;
